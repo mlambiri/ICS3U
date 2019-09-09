54e4286ebdc190f11cf767658548dba58edfcd9c
mlambiri <mlambiri@MihaW10>
2019-09-08 22:11:01 -0400

    changed result display

--- Homework/heightweight.c
+++ Homework/heightweight.c
@@ -1,31 +1,39 @@
-//Height,weight and age program.
+/*
+ * @author   mlambiri
+ * @date     Sep 8, 2019
+ * Height,weight and age program.
+ * Author: Michael C Lambiri
+ */
 
-//Stuff to remember
+
 #include <stdio.h>
+
+
 int main() {
 
-//Variables declared and initialized
-    int age = 0;
-    int height = 0;
-    float weight = 0;
+	//Variables declared and initialized
+	int age = 0;
+	int height = 0;
+	float weight = 0;
 
-//Questionnaire and recording results
-    printf("Please answer the following questions.");
-    printf("\n\nHow old are you?");
-    scanf("%d", &age);
-    printf("\nHow tall are you?");
-    scanf("%d", &height);
-    printf("\nWhat is your weight?");
-    scanf("%f", &weight);
-    //Displaying results
-    printf("\n\nage (yrs)");
-    printf("      height (cm)");
-    printf("    weight (kg)");
-    printf("\n--------       ----------     ----------- ");
-    printf("\n%-3d", age);
-    printf("              %-6d", height);
-    printf("         %-3.1f", weight);
-    //Fun add-ons
-    printf("\n\nThanks for using this program. Your data has been recorded\n\n");
+	//Questionnaire and recording results
+	printf("Please answer the following questions.");
+	printf("\n\nHow old are you? (yrs) ");
+	scanf("%d", &age);
+	printf("\nHow tall are you? (cm) ");
+	scanf("%d", &height);
+
+	printf("\nWhat is your weight? (kg) ");
+	scanf("%f", &weight);
+
+	//Displaying results
+	printf("\nage (yrs)      height (cm)    weight (kg)");
+	printf("\n--------       ----------     ----------- ");
+	printf("\n%-3d              %-6d         %-3.1f", age, height, weight);
+
+
+	//Fun add-ons
+	printf("\n\nThanks for using this program. Your data has been recorded!\n\n");
 }
+
 
