2050d386938f908f90370a2b541515f116cdd49b
mlambiri <mlambiri@MihaW10.localdomain>
2019-10-02 02:37:05 +0000

    added more play modes

--- Homework/Sticks.cpp
+++ Homework/Sticks.cpp
@@ -14,20 +14,56 @@
 
 int main () {
 
-    bool turnPlayer;
-    bool winLose;
     int stickNumber;
     int stickTaker;
     char startGame;
-    char turnOrder;
-    char enterAbsorber;
     int  minSticks = 10;
     bool pickOrder = false;
-    bool humanTurn = false;
+    bool playerOneTurn = false;
     bool correctPick = false;
+    bool playerOneHuman = false;
+    bool playerTwoHuman = false;
+    int  playerOneScore = 0;
+    int  playerTwoScore = 0;
+    int  gameType = 0;
 
     srand(time(0));
 
+
+    /*
+     * @author   mlambiri
+     * @date     Oct 1, 2019
+     *  The user has to first pick what type of game he wants
+     */
+    printf("Welcome to pick up sticks! Wanna play?\n");
+    printf("Pick one of the following 4 game types:\n");
+    printf("1) Human against Human\n");
+    printf("2) Human against Smart Computer\n");
+    printf("3) Dumb Computer against Human\n");
+    printf("4) Dumb Computer against Smart Computer\n");
+    printf(">");
+    scanf("%d", &gameType);
+    switch(gameType) {
+    case 1:
+        playerOneHuman = true;
+        playerTwoHuman = true;
+        break;
+    case 2:
+        playerOneHuman = true;
+        playerTwoHuman = false;
+        break;
+    case 3:
+        playerOneHuman = false;
+        playerTwoHuman = true;
+        break;
+    case 4:
+        playerOneHuman = false;
+        playerTwoHuman = false;
+        break;
+    default:
+        printf("Invalid Entry! Goodbye!!\n");
+        return 22;
+    }
     /*
      * @author   mlambiri
      * @date     Oct 1, 2019
@@ -35,35 +71,28 @@ int main () {
      */
 
     while (true) {
-        printf("Welcome to pick up sticks? Wanna play? Enter y to start or n to quit. > ");
+        printf("Enter y to start or n to quit. > ");
         scanf("%c", &startGame);
         if (startGame == 'y') {
 
             pickOrder = false;
-            humanTurn = false;
+            playerOneTurn = false;
             /*
              * @author   mlambiri
              * @date     Oct 1, 2019
-             *  The user has to pick if he wants to go first or second
+             *  Use a coin toss to figure out who starts the game
+             *
              */
 
-            while(pickOrder == false) {
-                printf("\nWould you like to go first or second? f for first s for second. > ");
-                scanf("%c", &turnOrder);
-                switch (turnOrder) {
-                case 'f':
-                    humanTurn = true;
-                    pickOrder = true;
-                    break;
-                case 's':
-                    humanTurn = false;
-                    pickOrder = true;
-                    break;
-                default:
-                    printf("\nPlease pick a turn order!\n");
+            playerOneTurn = rand()%2;
 
-                }
+            if(playerOneTurn == true) {
+                printf("Player One has the first turn.\n");
             }
+            else {
+                printf("Player Two has the first turn.\n");
+            }
+
             /*
              * @author   mlambiri
              * @date     Oct 1, 2019
@@ -88,23 +117,36 @@ int main () {
 
             while(true) {
                 printf("There are %d sticks remaining\n", stickNumber);
-                if(humanTurn == true) {
+                if(playerOneTurn == true) {
                     /*
                      * @author   mlambiri
                      * @date     Oct 1, 2019
                      *  Once the human has taken a turn it is the computer's turn
                      */
-                    humanTurn = false;
-                    correctPick = false;
-                    while(correctPick == false) {
-                        printf("Enter how many sticks you will pick up from 1-3. >");
-                        scanf("%d", &stickTaker);
-                        if((stickTaker >=1) && (stickTaker <= 3)) {
-                            correctPick = true;
+                    playerOneTurn = false;
+                    if(playerOneHuman == true) {
+                        correctPick = false;
+                        while(correctPick == false) {
+                            printf("[Player One] Pick some sticks (1-3)! >");
+                            scanf("%d", &stickTaker);
+                            if((stickTaker >=1) && (stickTaker <= 3)) {
+                                correctPick = true;
+                            }
+                            else {
+                                printf("Invalid number PLayer One! Try again!\n");
+                            }
                         }
-                        else {
-                            printf("Invalid number! Try again!\n");
-                        }
+
+                    }
+                    else {
+
+                        /*
+                         * @author   mlambiri
+                         * @date     Oct 1, 2019
+                         *  Computer picks at random 1 to 3 sticks
+                         */
+                        stickTaker = rand()%3+1;
+
                     }
                     if(stickNumber <= stickTaker) {
                         /*
@@ -113,11 +155,13 @@ int main () {
                          *  The human has picked the last stick. He loses!!
                          */
                         printf("************************************\n");
-                        printf("You have picked the last stick! Computer wins!!\n\n");
+                        printf("Player One picked the last stick! Player Two wins!!\n\n");
                         printf("************************************\n");
+                        playerTwoScore++;
                         break;
                     }
                     else {
+                        printf("Player One has picked %d sticks!\n", stickTaker);
                         stickNumber -= stickTaker;
                     }
 
@@ -127,61 +171,69 @@ int main () {
                      * @date     Oct 1, 2019
                      *  Once the computer has taken a turn it is the human's turn
                      */
-                    humanTurn = true;
+                    playerOneTurn = true;
+                    if(playerOneHuman == true) {
+                        correctPick = false;
+                        while(correctPick == false) {
+                            printf("[Player Two] Pick some sticks (1-3)! >");
+                            scanf("%d", &stickTaker);
+                            if((stickTaker >=1) && (stickTaker <= 3)) {
+                                correctPick = true;
+                            }
+                            else {
+                                printf("Invalid number Player Two! Try again!\n");
+                            }
+                        }
 
-                    /*
-                     * @author   mlambiri
-                     * @date     Oct 1, 2019
-                     *  Computer picks at random 1 to 3 sticks
-                     */
-                    // stickTaker = rand()%3+1;
-
-                    /*
-                     * @author   mlambiri
-                     * @date     Oct 1, 2019
-                     *  To improve the algorithm the computer can check
-                     *  how many sticks there are left
-                     *  If there are 2 left then pick 1
-                     *  If there are 3 left then pick 2
-                     *  If there are 4 left then pick 3
-                     *  If there are 5 left then pick 1
-                     *  If there are 6 left computer loses
-                     *  If there are 7 left pick 1
-                     *  If there are 8 left pick pick 2
-                     *  If there are 9 left pick 3
-                     *  If there are 10 left pick 1
-                     *
-                     *  else pick at random between 1 to 3
-                     */
-                    switch (stickNumber) {
-                    case 2:
-                        stickTaker = 1;
-                        break;
-                    case 3:
-                        stickTaker = 2;
-                        break;
-                    case 4:
-                        stickTaker = 3;
-                        break;
-                    case 5:
-                        stickTaker = 1;
-                        break;
-                    case 7:
-                        stickTaker = 1;
-                        break;
-                    case 8:
-                        stickTaker = 2;
-                        break;
-                    case 9:
-                        stickTaker = 3;
-                        break;
-                    case 10:
-                        stickTaker = 1;
-                        break;
-                    default:
-                        stickTaker = rand()%3+1;
                     }
+                    else {
 
+                        /*
+                         * @author   mlambiri
+                         * @date     Oct 1, 2019
+                         *  To improve the algorithm the computer can check
+                         *  how many sticks there are left
+                         *  If there are 2 left then pick 1
+                         *  If there are 3 left then pick 2
+                         *  If there are 4 left then pick 3
+                         *  If there are 5 left then pick 1
+                         *  If there are 6 left computer loses
+                         *  If there are 7 left pick 1
+                         *  If there are 8 left pick pick 2
+                         *  If there are 9 left pick 3
+                         *  If there are 10 left pick 1
+                         *
+                         *  else pick at random between 1 to 3
+                         */
+                        switch (stickNumber) {
+                        case 2:
+                            stickTaker = 1;
+                            break;
+                        case 3:
+                            stickTaker = 2;
+                            break;
+                        case 4:
+                            stickTaker = 3;
+                            break;
+                        case 5:
+                            stickTaker = 1;
+                            break;
+                        case 7:
+                            stickTaker = 1;
+                            break;
+                        case 8:
+                            stickTaker = 2;
+                            break;
+                        case 9:
+                            stickTaker = 3;
+                            break;
+                        case 10:
+                            stickTaker = 1;
+                            break;
+                        default:
+                            stickTaker = rand()%3+1;
+                        }
+                    }
                     if(stickNumber <= stickTaker) {
                         /*
                          * @author   mlambiri
@@ -189,12 +241,13 @@ int main () {
                          *  The human has picked the last stick. He loses!!
                          */
                         printf("************************************\n");
-                        printf("Computer has picked the last %d sticks! You win!!\n\n", stickNumber);
+                        printf("Player Two has picked the last %d sticks! Player One wins!!\n\n", stickNumber);
                         printf("************************************\n");
+                        playerOneScore++;
                         break;
                     }
                     else {
-                        printf("Computer has picked %d sticks!\n", stickTaker);
+                        printf("Player Two has picked %d sticks!\n", stickTaker);
                         stickNumber -= stickTaker;
                     }
                 }
@@ -206,6 +259,7 @@ int main () {
         } else {
             printf("\nI don't understand what you want!\n");
         }
+        printf("Score *** Player One: %d          Player Two: %d\n\n", playerOneScore, playerTwoScore);
     }
 
 }
